e0eb98406649129344ccb10c353e7a78b04c7603
unknown <abdelhamidzdh@gmail.com>
2026-01-01 18:00:44 +0100

    Changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ let getFormattedDate = () => {
   let date = new Date();
   let year = date.getFullYear();
   let month = String(date.getMonth() + 1).padStart(2, '0');
-  let day = String(date.getDate() + 1).padStart(2, '0');
+  let day = String(date.getDate()).padStart(2, '0');
   return `${year}-${month}-${day}`;
 }
 let SUPPORTED_LEAGUES_IDS = [1, 2, 3, 6, 8, 12, 19, 20, 29, 30, 31, 32, 33, 34, 37, 39, 45, 48, 61, 66, 78, 135, 137, 140, 143, 307, 490, 587, 848, 920, 1186]
